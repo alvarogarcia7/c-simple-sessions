ff3c1e03ff1cf4e87a2e5632685c7f924e1fcdf5
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2020-05-01 13:27:38 +0200

    Refactor: implement size + create

--- linked_list/prod.c
+++ linked_list/prod.c
@@ -1,10 +1,14 @@
-#include <ntsid.h>
+#include <stdlib.h>
 #include "prod.h"
 
-int_list *list_new(){
-    return 0;
+int_list *list_new() {
+    return calloc(1, sizeof(int_list));
 }
 
-int64_t list_size(int_list *list){
-    return 0;
+int64_t list_size(int_list *list) {
+    int64_t result = 0;
+    while (list->next != NULL) {
+        result++;
+    }
+    return result;
 }

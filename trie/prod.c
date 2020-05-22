80c4c7c51d92c7cd2a7e3a7453367f8bd263486d
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2020-05-22 20:39:59 +0200

    [Trie] Refactor: remove redundant else branch

--- trie/prod.c
+++ trie/prod.c
@@ -28,64 +28,64 @@ void trie_add(trie_t *trie, char *string) {
     if (trie->string == NULL) {
         trie->string = string;
         trie->children = 0;
-    } else {
-        unsigned int matching_characters = strspn(trie->string, string);
-        if (matching_characters == strlen(string)) {
-            //need to split trie
-            unsigned int rest_size = strlen(string) - matching_characters;
-            char *first_part = calloc(matching_characters + 1, sizeof(char));
-            strncpy(first_part, &(trie->string[0]), matching_characters);
+        return;
+    }
+    unsigned int matching_characters = strspn(trie->string, string);
+    if (matching_characters == strlen(string)) {
+        //need to split trie
+        unsigned int rest_size = strlen(string) - matching_characters;
+        char *first_part = calloc(matching_characters + 1, sizeof(char));
+        strncpy(first_part, &(trie->string[0]), matching_characters);
 //            printf("'%s'\n", first_part);
-            char *second_part = calloc(rest_size + 1, sizeof(char));
-            strncpy(second_part, &(trie->string[matching_characters]), strlen(string) + 1);
+        char *second_part = calloc(rest_size + 1, sizeof(char));
+        strncpy(second_part, &(trie->string[matching_characters]), strlen(string) + 1);
 //            printf("'%s'\n", second_part);
-            trie->string = first_part;
-            trie->children++;
-            trie_t *next = trie_new();
-            next->string = second_part;
-            trie->next = calloc(1, sizeof(trie_t *));
-            trie->next[0] = next;
-        } else if (matching_characters == strlen(trie->string)) {
-            //need to go deeper
-            unsigned int rest_size = strlen(string) - matching_characters;
-            char *second_part = calloc(rest_size + 1, sizeof(char));
-            strncpy(second_part, &(string[matching_characters]), rest_size);
-            bool inserted = false;
-            for (int i = 0; i < trie->children; i++) {
-                if (strspn(trie->next[i]->string, second_part) > 0) {
-                    trie_add(trie->next[i], second_part);
-                    inserted = true;
-                }
+        trie->string = first_part;
+        trie->children++;
+        trie_t *next = trie_new();
+        next->string = second_part;
+        trie->next = calloc(1, sizeof(trie_t *));
+        trie->next[0] = next;
+    } else if (matching_characters == strlen(trie->string)) {
+        //need to go deeper
+        unsigned int rest_size = strlen(string) - matching_characters;
+        char *second_part = calloc(rest_size + 1, sizeof(char));
+        strncpy(second_part, &(string[matching_characters]), rest_size);
+        bool inserted = false;
+        for (int i = 0; i < trie->children; i++) {
+            if (strspn(trie->next[i]->string, second_part) > 0) {
+                trie_add(trie->next[i], second_part);
+                inserted = true;
             }
-            if (!inserted) {
-                trie_add(trie, second_part);
-            }
-        } else if (matching_characters == 0) {
-            append_as_child(trie, string);
-        } else if (matching_characters <= strlen(string)) {
-            //need to split trie
-            unsigned int rest_size = strlen(string) - matching_characters;
-            char *shared_part = calloc(matching_characters + 1, sizeof(char));
-            strncpy(shared_part, &(trie->string[0]), matching_characters);
-            char *rest_of_first = calloc(rest_size + 1, sizeof(char));
-            strncpy(rest_of_first, &(trie->string[matching_characters]), rest_size);
-
-
-            char *second_part = calloc(rest_size + 1, sizeof(char));
-            strncpy(second_part, &(string[matching_characters]), rest_size);
-            trie_t **previousNext = trie->next;
-
-            trie->string = shared_part;
-            trie->children = 2;
-            trie->next = calloc(2, sizeof(trie_t *));
-            trie->next[0] = trie_new();
-            trie->next[0]->string = rest_of_first;
-            trie->next[0]->next = calloc(1, sizeof(trie_t *));
-            trie->next[0]->next = previousNext;
-
-            trie->next[1] = trie_new();
-            trie->next[1]->string = second_part;
         }
+        if (!inserted) {
+            trie_add(trie, second_part);
+        }
+    } else if (matching_characters == 0) {
+        append_as_child(trie, string);
+    } else if (matching_characters <= strlen(string)) {
+        //need to split trie
+        unsigned int rest_size = strlen(string) - matching_characters;
+        char *shared_part = calloc(matching_characters + 1, sizeof(char));
+        strncpy(shared_part, &(trie->string[0]), matching_characters);
+        char *rest_of_first = calloc(rest_size + 1, sizeof(char));
+        strncpy(rest_of_first, &(trie->string[matching_characters]), rest_size);
+
+
+        char *second_part = calloc(rest_size + 1, sizeof(char));
+        strncpy(second_part, &(string[matching_characters]), rest_size);
+        trie_t **previousNext = trie->next;
+
+        trie->string = shared_part;
+        trie->children = 2;
+        trie->next = calloc(2, sizeof(trie_t *));
+        trie->next[0] = trie_new();
+        trie->next[0]->string = rest_of_first;
+        trie->next[0]->next = calloc(1, sizeof(trie_t *));
+        trie->next[0]->next = previousNext;
+
+        trie->next[1] = trie_new();
+        trie->next[1]->string = second_part;
     }
 }
 

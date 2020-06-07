0bcecd5413cf687983daa0d602ed8a10219152ea
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2020-06-07 17:48:38 +0200

    [CMocka] Create sample integration (like a skeleton) without any business feature

---
+++ cmocka_test/fizzBuzzTest.c
@@ -0,0 +1,36 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
+
+#include "prod.h"
+
+//void Test_number_multiple_of_3_is_translated_to_Fizz(CuTest *tc)
+//{
+//	CuAssertStrEquals(tc, "Fizz", fizzBuzz(6));
+//	CuAssertStrEquals(tc, "Fizz", fizzBuzz(3));
+//	CuAssertStrEquals(tc, "Fizz", fizzBuzz(9));
+//	CuAssertStrEquals(tc, "Fizz", fizzBuzz(12));
+//}
+//
+//void Test_number_5_is_translated_to_Buzz(CuTest *tc)
+//{
+//	CuAssertStrEquals(tc, "Buzz", fizzBuzz(5));
+//}
+
+//static void Test_number_15_is_translated_to_FizzBuzz(void **state)
+//{
+//    assert_string_equal("FizzBuzz", fizzBuzz(15));
+//}
+
+/* A test case that does nothing and succeeds. */
+static void null_test_success(void **state) {
+    (void) state; /* unused */
+}
+int main(void) {
+    const struct CMUnitTest tests[] = {
+            cmocka_unit_test(null_test_success),
+//            cmocka_unit_test(Test_number_15_is_translated_to_FizzBuzz)
+    };
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}

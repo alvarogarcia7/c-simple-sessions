85eaf97cb09fbf7aca3c89522b007559fb4bbfd7
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2020-06-07 17:52:01 +0200

    [CMocka] Finish converting tests from CUnit to CMocka

--- cmocka_test/fizzBuzzTest.c
+++ cmocka_test/fizzBuzzTest.c
@@ -5,14 +5,13 @@
 
 #include "prod.h"
 
-//void Test_number_multiple_of_3_is_translated_to_Fizz(CuTest *tc)
-//{
-//	CuAssertStrEquals(tc, "Fizz", fizzBuzz(6));
-//	CuAssertStrEquals(tc, "Fizz", fizzBuzz(3));
-//	CuAssertStrEquals(tc, "Fizz", fizzBuzz(9));
-//	CuAssertStrEquals(tc, "Fizz", fizzBuzz(12));
-//}
-//
+void Test_number_multiple_of_3_is_translated_to_Fizz()
+{
+	assert_string_equal("Fizz", fizzBuzz(6));
+	assert_string_equal("Fizz", fizzBuzz(3));
+	assert_string_equal("Fizz", fizzBuzz(9));
+	assert_string_equal("Fizz", fizzBuzz(12));
+}
 
 void Test_number_5_is_translated_to_Buzz()
 {
@@ -32,7 +31,8 @@ int main(void) {
     const struct CMUnitTest tests[] = {
             cmocka_unit_test(null_test_success),
             cmocka_unit_test(Test_number_15_is_translated_to_FizzBuzz),
-            cmocka_unit_test(Test_number_5_is_translated_to_Buzz)
+            cmocka_unit_test(Test_number_5_is_translated_to_Buzz),
+            cmocka_unit_test(Test_number_multiple_of_3_is_translated_to_Fizz)
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }

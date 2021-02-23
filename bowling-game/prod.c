5ae4234bdca1bfa8f9079ca6b4ba848cd5f7a10b
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2021-02-23 11:56:25 +0400

    Refactor: remove comment

--- bowling-game/prod.c
+++ bowling-game/prod.c
@@ -23,8 +23,6 @@ int score_calc(char *input){
         if (input[1] == '-') {
             return 0;
         }
-        // 19 => 1/, score() = 10
-        // 18 => correct, score() = 9
         if (numberCharToInt(input[1]) >= 1 && numberCharToInt(input[1]) <= 9) {
             return numberCharToInt(input[1]);
         }

7476363be7098a6c4ca9f50b5bbc72a888503c8b
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2021-02-23 11:32:04 +0400

    Refactor: remove tests that were confusing

--- bowling-game/tests/test.c
+++ bowling-game/tests/test.c
@@ -11,12 +11,9 @@ static void canary_test(void **state) {
     assert_int_equal(0, 0);
 }
 
-static void score_for_nine_pins_and_a_miss(void **state){
-    assert_int_equal(9, score_calc("9-"));
-}
-
 static void score_when_the_second_is_a_miss(void **state){
     assert_int_equal(5, score_calc("5-"));
+    assert_int_equal(7, score_calc("7-"));
 }
 
 static void score_when_both_rolls_throw_pins(void **state){
@@ -34,7 +31,6 @@ static void score_when_strike(void **state){
 int main(void) {
     const struct CMUnitTest tests[] = {
             cmocka_unit_test(canary_test)
-            , cmocka_unit_test(score_for_nine_pins_and_a_miss)
             , cmocka_unit_test(score_when_the_second_is_a_miss)
             , cmocka_unit_test(score_when_both_rolls_throw_pins)
             , cmocka_unit_test(score_when_spare)

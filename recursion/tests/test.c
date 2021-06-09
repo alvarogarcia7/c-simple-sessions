e5b2c877e20c79b3bacc9bffe148998c5ef4e20c
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2021-06-09 14:34:00 +0400

    [Recursion] Red: sum array two elements

--- recursion/tests/test.c
+++ recursion/tests/test.c
@@ -17,6 +17,12 @@ static void test_sum_array_one_element(void **state){
     assert_int_equal(2, sum(array, length));
 }
 
+static void test_sum_array_two_elements(void **state){
+    int array[] = {2, 3};
+    int length = sizeof(array) / sizeof(int);
+    assert_int_equal(5, sum(array, length));
+}
+
 /* A test case that does nothing and succeeds. */
 static void canary_test(void **state) {
     (void) state; /* unused */
@@ -27,6 +33,7 @@ int main(void) {
             cmocka_unit_test(canary_test)
             , cmocka_unit_test(test_sum_empty_array)
             , cmocka_unit_test(test_sum_array_one_element)
+            , cmocka_unit_test(test_sum_array_two_elements)
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }

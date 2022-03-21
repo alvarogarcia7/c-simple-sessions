44de78ec15870512a9a7fdf3c74ff622e34093ee
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2022-03-21 19:12:46 +0400

    [ThinkingDrivenTesting] [7.5.5] Refactor: prefer using a single return (for comparing multiple hypotheses)

--- thinking-driven-testing/prod.c
+++ thinking-driven-testing/prod.c
@@ -11,23 +11,25 @@ int find_the_rule_7_5_5(int i) {
 
 //    printf("%d %d %d %d\n", digits[0], digits[1], digits[2], digits[3]);
     bool all_same_digits = digits[0] == digits[1] && digits[0] == digits[2] && digits[0] == digits[3];
+    int result = 0;
     if (7111 == i || 2172 == i || 3213 == i) {
-        return 0;
+        result = 0;
     } else if (8913 == i || 6855 == i) {
-        return 3;
+        result = 3;
     } else if (8809 == i) {
-        return 6;
+        result = 6;
     } else if (8096 == i || 9881 == i) {
-        return 5;
+        result = 5;
     } else if (9313 == i || 7756 == i) {
-        return 1;
+        result = 1;
     } else if (7662 == i){
-        return 2;
+        result = 2;
     } else if (all_same_digits) {
         if (6 == digits[0] || 0 == digits[0] || 9 == digits[0]) {
-            return 4;
+            result = 4;
+        } else {
+            result = 0;
         }
-        return 0;
     }
-    return 0;
+    return result;
 }

163cdb39c1bb050a99c65f10f9494160c48120ff
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2022-03-21 16:31:17 +0400

    [ThinkingDrivenTesting] [7.5.5] Refactor: make the digits const, compare all digits

--- thinking-driven-testing/prod.c
+++ thinking-driven-testing/prod.c
@@ -2,15 +2,16 @@
 #include <printf.h>
 
 int find_the_rule_7_5_5(int i) {
-    int digits[4] = {0};
-    digits[0] = (i / 1000) % 10;
-    digits[1] = (i / 100) % 10;
-    digits[2] = (i / 10) % 10;
-    digits[3] = i % 10;
+    const int digits[4] = {
+            (i / 1000) % 10,
+            (i / 100) % 10,
+            (i / 10) % 10,
+            i % 10};
+
 //    printf("%d %d %d %d\n", digits[0], digits[1], digits[2], digits[3]);
     if (7111 == i || 2172 == i) {
         return 0;
-    } else if (6666 == i) {
+    } else if (6666 == i && (digits[0] == digits[1] && digits[0] == digits[2] && digits[0] == digits[3])) {
         return 4;
     }
     return 6;

e8f32ed92be54cd6e4f007890877063ab079b143
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2022-03-21 16:32:34 +0400

    [ThinkingDrivenTesting] [7.5.5] Green

--- thinking-driven-testing/prod.c
+++ thinking-driven-testing/prod.c
@@ -13,6 +13,8 @@ int find_the_rule_7_5_5(int i) {
         return 0;
     } else if (6666 == i && (digits[0] == digits[1] && digits[0] == digits[2] && digits[0] == digits[3])) {
         return 4;
+    } else if (1111 == i && (digits[0] == digits[1] && digits[0] == digits[2] && digits[0] == digits[3])) {
+        return 0;
     }
     return 6;
 }

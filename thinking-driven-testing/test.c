0eaf77df7795acdb499f9540c2b9a1f7a0e1c109
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2022-03-21 16:21:20 +0400

    [ThinkingDrivenTesting] [7.5.5] Red: Case 1

--- thinking-driven-testing/test.c
+++ thinking-driven-testing/test.c
@@ -23,9 +23,15 @@ static MunitResult test_7_5_5_case0(){
     return MUNIT_OK;
 }
 
+static MunitResult test_7_5_5_case1(){
+    munit_assert_int(find_the_rule_7_5_5(7111), ==, 0);
+    return MUNIT_OK;
+}
+
 static MunitTest tests[] = {
         {(char *) "canary", canary_test, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
         {(char *) "7.5.5_case0", test_7_5_5_case0, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
+        {(char *) "7.5.5_case1", test_7_5_5_case1, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
         {NULL, NULL,                     NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}
 };
 

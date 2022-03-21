c13598e4530e89ee0cfcdb34e67b4e0548170814
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2022-03-21 17:39:54 +0400

    [ThinkingDrivenTesting] [7.5.5] Refactor: extract variable for all_same_digits

--- thinking-driven-testing/prod.c
+++ thinking-driven-testing/prod.c
@@ -1,5 +1,6 @@
 
 #include <printf.h>
+#include <stdbool.h>
 
 int find_the_rule_7_5_5(int i) {
     const int digits[4] = {
@@ -9,11 +10,12 @@ int find_the_rule_7_5_5(int i) {
             i % 10};
 
 //    printf("%d %d %d %d\n", digits[0], digits[1], digits[2], digits[3]);
+    bool all_same_digits = digits[0] == digits[1] && digits[0] == digits[2] && digits[0] == digits[3];
     if (7111 == i || 2172 == i || 3213 == i) {
         return 0;
-    } else if (6666 == i && (digits[0] == digits[1] && digits[0] == digits[2] && digits[0] == digits[3])) {
+    } else if (6666 == i && all_same_digits) {
         return 4;
-    } else if (1111 == i && (digits[0] == digits[1] && digits[0] == digits[2] && digits[0] == digits[3])) {
+    } else if (1111 == i && all_same_digits) {
         return 0;
     }
     return 6;
